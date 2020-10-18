a0d2fb857673a572b9d19c413a7a973140c2f06c
Todd Murdoch <TheClash090@gmail.com>
2020-10-17 21:00:41 -0700

    finished app.get functions to generate index.html and note.html

--- server.js
+++ server.js
@@ -5,21 +5,21 @@ const path = require ("path");
 const PORT = 3000;
 
 let app =express();
-let db = require ("./db/db.json");
+let db = require ("./Develop/db/db.json");
 
 app.use(express.json());
 
 app.use(express.urlencoded({ extended: true }));
 
 const dbNotes = JSON.parse(
-    fs.readFileSync(path.join(__dirname, "/db/db.json"), (err, data) => {
+    fs.readFileSync(path.join(__dirname, "./Develop/db/db.json"), (err, data) => {
         if (err) throw err;
     })
 );
 
 const dbUpdate = dbNotes => {
     fs.writeFileSync(
-        path.join(__dirname, "/db/db.json"),
+        path.join(__dirname, "./Develop/db/db.json"),
         JSON.stringify(dbNotes),
         err => {
             if (err) throw err;
@@ -29,19 +29,19 @@ const dbUpdate = dbNotes => {
 
 // app.get requests for html pages //
 app.get("/assets/css/styles.css", (req, res) => {
-    res.sendFile(path.join(__dirname, "/public/assets/css/styles.css"));
+    res.sendFile(path.join(__dirname, "./Develop/public/assets/css/styles.css"));
 });
 
 app.get("/assets/js/index.js", (req,res) => {
-    res.sendFile(path.join(__dirname, "/public/assets/js/index.js"));
+    res.sendFile(path.join(__dirname, "./Develop/public/index.html"));
 });
 
 app.get("/", (req,res) => {
-    res.sendFile(path.join(__dirname, "public/index.html"));
+    res.sendFile(path.join(__dirname, "./Develop/public/index.html"));
 });
 
 app.get("/notes", (req,res) => {
-    res.sendFile(path.join(__dirname, "/public/notes.html"));
+    res.sendFile(path.join(__dirname, "./Develop/public/notes.html"));
 });
 
 app.get("/api/notes", (req,res) => {
@@ -49,9 +49,9 @@ app.get("/api/notes", (req,res) => {
 });
 
 
-
+// add app push //
 
 
 app.listen(PORT, function () {
-    console.log("https://localhost:" + PORT);
+    console.log("https://localhost:" + PORT + " is online");
 })
