301c8788a38e42865853cf7e2ed0f3af82ca357e
Todd Murdoch <TheClash090@gmail.com>
2020-10-26 11:44:01 -0700

    corrected missing dependencies.

--- server.js
+++ server.js
@@ -1,78 +1,42 @@
 const express = require("express");
 const fs = require ("fs");
 const path = require ("path");
+const app = express();
 
-const PORT = 3000;
 
-let app =express();
-let db = require ("./Develop/db/db.json");
+const PORT = process.env.PORT || 3000;
 
+
+app.use(express.urlencoded({ extended: true}));
 app.use(express.json());
 
-app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
 
-app.use(express.static(__dirname + "/public"));
-
-const dbNotes = JSON.parse(
-    fs.readFileSync(path.join(__dirname, "./Develop/db/db.json"), (err, data) => {
-        if (err) throw err;
-    })
-);
-
-const dbUpdate = dbNotes => {
-    fs.writeFileSync(
-        path.join(__dirname, "./Develop/db/db.json"),
-        JSON.stringify(dbNotes),
-        err => {
-            if (err) throw err;
-        }
-    );
-};
-
-// app.get requests for html pages //
-app.get("./Develop/public/assets/css/styles.css", (req, res) => {
-    res.sendFile(path.join(__dirname, "./Develop/public/assets/css/styles.css"));
+app.get("/notes", (request, response) => {
+    response.sendFile(path.join(__dirname + "/notes.html"));
 });
 
-app.get("./Develop/public/index.js", (req,res) => {
-    res.sendFile(path.join(__dirname, "./Develop/public/index.js"));
+app.get("/api/notes", (request, response) => {
+    response.sendFile(path.join(__dirname + "/db/db.json"));
 });
 
-app.get("/", (req,res) => {
-    res.sendFile(path.join(__dirname, "./Develop/public/index.html"));
+
+app.post("/api/notes", (request, response) => {
+    let notes = JSON.parse(fs.readFileSync(path.join(__dirname + "/db/db.json")));
+    notes.push(request.body);
+    fs.writeFileSync(path.join(__dirname + "/db/db.json"), JSON.stringify(newNotes));
+    response.end();
 });
 
-app.get("/notes", (req,res) => {
-    res.sendFile(path.join(__dirname, "./Develop/public/notes.html"));
-});
-
-app.get("/api/notes", (req,res) => {
-    return res.json(dbNotes);
-});
-
-// request for posting and deleting the notes //
-
-app.post('/api/notes', (req,res) => {
-    let newNote = req.body;
-    let id = dbNotes.length;
-    newNote.id = id + 1;
-    dbNotes.push(newNote);
-    dbUpdate(dbNotes);
-    return res.json(dbNotes);
-});
-
-// this deletes the notes //
-app.delete("/api/notes/:id", (req,res) => {
-    let id = req.params.id;
-    let x = 1;
-    delete dbNotes[id - 1];
-    dbUpdate(dbNotes);
-    res.send(dbNotes);
-});
+app.delete('/api/notes/:id', (request, response) => {
+    let notes = JSON.parse(fs.readFileSync(path.join(__dirname + "/db/db.json")));
+    let newNotes = notes.filter(note => note.id !== request.params.id);
+    fs.writeFileSync(path.join(__dirname + "/index.html"));
+})
 
 
 // listener to PORT 3000 *todos: change to Proccess.env when deploying to heroku//
 
 app.listen(PORT, function () {
-    console.log("https://localhost:" + PORT + " is listening");
+    console.log("Online" + PORT);
 })
